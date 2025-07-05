2093c0e7bf2c1c4825147088b1a9fbc278375ea9
Utsav-56 <utsavpokhrel200@gmail.com>
2025-07-05 22:15:26 +0545

    Removed the errors

--- boxlayout.java
+++ boxlayout.java
@@ -1,13 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 
-class BoxEg{
-    public static void public static void main(String[] args) {
-        JFrame body = new.JFrame("Wish Java becomes shorter!");
-        body.setLayout(newBoxLayout(body,BoxLayout.X_AXIS));
+class BoxEg {
+    public static void main(String[] args) {
+        JFrame body = new JFrame("Wish Java becomes shorter!");
+        body.setLayout(new BoxLayout(body, BoxLayout.X_AXIS));
 
-        for(int i=0; i<3; i++){
-            body.add(new JButton(i));
+        for (int i = 0; i < 3; i++) {
+            body.add(new JButton("" + i));
         }
         body.setVisible(true);
     }
--- flowlayout.java
+++ flowlayout.java
@@ -2,16 +2,17 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import javax.swing.*;
 
-class FlowLayoutEg{
+class FlowLayoutEg {
     public static void main(String[] args) {
-        JFrame body=new JFrame("Still is too verbose why!");
+        JFrame body = new JFrame("Still is too verbose why!");
         body.setLayout(new FlowLayout());
         int i = 10;
-        do { 
-            body.add(new JButton("Btn" +i));
-        } while (--i>0);
-        Frame.setSize(300,500);
-        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Frame.setVisible(true);
+        do {
+            body.add(new JButton("Btn" + i));
+        } while (--i > 0);
+
+        body.setSize(300, 500);
+        body.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        body.setVisible(true);
     }
 }
